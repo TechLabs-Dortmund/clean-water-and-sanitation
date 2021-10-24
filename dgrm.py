9a5383ef9b396348fc845393d8a2728a640ac4f6
Florian Zimmer <75251714+TheMerphin@users.noreply.github.com>
2021-10-24 12:37:45 +0200

    Added project files

---
+++ dgrm.py
@@ -0,0 +1,256 @@
+import sys
+import webbrowser
+from enum import Enum
+import folium
+import geojson
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+class Type(Enum):
+    SCATTER = 0
+    BAR = 1
+    LINE = 2
+    MAP = 3
+
+
+class Diagram:
+
+    def __init__(self, type: Type, df_s, df_w, df_h, var_s, var_w, var_h):
+        self.type = type
+        self.dfSanitation = df_s
+        self.dfWater = df_w
+        self.dfHygiene = df_h
+        self.variables_sanitation = var_s
+        self.variables_water = var_w
+        self.variables_hygiene = var_h
+
+    # returns a dataframe containing the average worldwide values per year
+    def get_average_over_time(self, variable="Population (thousands)"):
+        index = -1
+        if variable in self.variables_water:
+            df = self.dfWater
+            index = self.variables_water[variable]
+        elif variable in self.variables_hygiene:
+            df = self.dfHygiene
+            index = self.variables_hygiene[variable]
+        elif variable in self.variables_sanitation:
+            df = self.dfSanitation
+            index = self.variables_sanitation[variable]
+        else:
+            print("ERROR: variable not found!")
+        if index == -1:
+            print("ERROR: variables index out of bound.")
+        averages = []
+        for year in range(2000, 2021):
+            temp_averages = df[df.iloc[:, 2] == year]
+            temp_averages = temp_averages.iloc[:, index]
+            temp_averages = sum(temp_averages) / len(temp_averages)
+            averages.append(temp_averages)
+        list = ["average" for i in range(0, 21)]
+        df_averages = pd.DataFrame(data=averages)
+        df_averages["country"] = list
+        df_averages["year"] = range(2000, 2021)
+        df_averages.columns = [variable, 'country', 'year']
+        return df_averages
+
+    # returns the iso of a single country
+    def get_iso(self, country):
+        if country == "average":
+            return "AVG"
+        df = self.dfSanitation.iloc[:, 0:2]
+        iso = ""
+        index = 0
+        while iso == "":
+            if df.iloc[index, 0] == country:
+                iso = df.iloc[index, 1]
+            index = index + 1
+        return iso
+
+    # returns a dataframe containing only the values requested from a single variable and nation
+    def get_data(self, variable, nation):
+        if variable in self.variables_water:
+            df = self.dfWater
+        elif variable in self.variables_hygiene:
+            df = self.dfHygiene
+        elif variable in self.variables_sanitation:
+            df = self.dfSanitation
+        else:
+            print("ERROR: variable " + variable + " not found! Did you maybe misspell it?")
+            sys.exit()
+        data = df[df.iloc[:, 0] == nation]
+        if variable in self.variables_water:
+            data = data.iloc[:, self.variables_water[variable]]
+        elif variable in self.variables_hygiene:
+            data = data.iloc[:, self.variables_hygiene[variable]]
+        elif variable in self.variables_hygiene:
+            data = data.iloc[:, self.variables_sanitation[variable]]
+        else:
+            print("ERROR: variable not found!")
+        return data
+
+    # return a dataframe containing the data for a single nation for a single variable over time
+    def get_data_for_nation(self, variable="Population (thousands)", nation="Madagascar"):
+        data = self.get_data(variable=variable, nation=nation)
+        list_countries = [nation for i in range(0, 21)]
+        iso = self.get_iso(nation)
+        list_iso = [iso for i in range(0, 21)]
+        result = pd.DataFrame(data)
+        result["country"] = list_countries
+        result["year"] = range(2000, 2021)
+        result["ISO_A3"] = list_iso
+        result.columns = [variable, "country", "year", "ISO_A3"]
+        return result
+
+    # returns a dataframe containing only the values requested from a two variable and nation
+    # NOTE: needs two variables, less will throw an error, more variables will be ignored
+    def get_double_data(self, variables, nation):
+        values_a = self.get_data(variable=variables[0], nation=nation)
+        values_b = self.get_data(variable=variables[1], nation=nation)
+        list_countries = [nation for i in range(0, 21)]
+        iso = self.get_iso(nation)
+        list_iso = [iso for i in range(0, 21)]
+        result = pd.concat([values_a, values_b], axis=1, join='inner')
+        result["country"] = list_countries
+        result["year"] = range(2000, 2021)
+        result["ISO_A3"] = list_iso
+        result.columns = [variables[0], variables[1], "country", "year", "ISO_A3"]
+        return result
+
+    # returns all the data from every country for a specific variable and year
+    # needed for the data for maps
+    def get_world_data(self, variable="Population (thousands)", year=2020):
+        countries = self.dfSanitation.iloc[:, 0].unique().tolist()
+        df = self.create_dataframe(variable=variable, nations=countries)
+        result = df[df.iloc[:, 2] == year]
+        return result
+
+    # creates a plot showing one variable for one country over time
+    def print_single_variable_of_single_country_over_time(self, variable: str, country: str):
+        dfs = Diagram.get_data_for_nation(self, variable=variable, nation=country)
+        x_axis = dfs[dfs.iloc[:, 0] == country]
+        y_axis = x_axis
+        x_axis = x_axis.iloc[:, 2]
+        plt.title(country)
+        plt.ylabel(variable)
+        plt.xlabel("Year")
+        plt.xticks(range(2000, 2021))
+        plt.plot(x_axis, y_axis)
+        plt.show()
+
+    # creates and returns a dataframe containing all the data for one specific variable for multiple countries
+    def create_dataframe(self, variable, nations):
+        dataframes = []
+        for nation in nations:
+            if nation == "average":
+                dataframes.append(Diagram.get_average_over_time(self, variable))
+            else:
+                dataframes.append(Diagram.get_data_for_nation(self, variable, nation))
+        result = pd.concat(dataframes)
+        return result
+
+    # creates and returns a dataframe containing all the data for multiple of variables and countries
+    def multi_dataframe(self, variables, nations):
+        dataframes = []
+        for index in range(0, len(variables)):
+            dataframes.append(pd.DataFrame())
+        frame = pd.DataFrame()
+        for index in range(0, len(dataframes)):
+            for nation in nations:
+                if nation == "average":
+                    df = self.get_average_over_time(variables[index])
+                    if frame.empty:
+                        frame = df
+                    else:
+                        frame = pd.concat([frame, df], axis=1, join='inner', copy=False)
+                    frame = frame.loc[:, ~frame.columns.duplicated()]
+                    if dataframes[index].size == 0:
+                        dataframes[index] = frame
+                    else:
+                        dataframes[index] = pd.concat([dataframes[index].append(frame)])
+                else:
+                    if dataframes[index].size == 0:
+                        dataframes[index] = self.get_double_data(variables, nation)
+                    else:
+                        df = self.get_double_data(variables, nation)
+                        dataframes[index] = pd.concat([dataframes[index].append(df)])
+        result = pd.concat(dataframes)
+        return result
+
+    # creates a plot comparing one variable of one country against the average over time
+    def lineplot_single_variable_over_time(self, variable, nations: [str]):
+        dataframe = Diagram.create_dataframe(self, variable, nations)
+        sns.lineplot(data=dataframe, x="year", y=variable, hue="country")
+        plt.xticks(range(2000, 2021))
+        plt.show()
+
+    # creates a barplot of a single variable on y-Axis and years on x-Axis
+    def barplot_variable_over_time(self, variable: str, nations: [str]):
+        dataframe = Diagram.create_dataframe(self, variable, nations)
+        sns.barplot(data=dataframe, x="year", y=variable, hue="country")
+        plt.show()
+
+    def scatterplot_two_variables(self, var_x: str, var_y: str, nations: [str]):
+        df = self.multi_dataframe((var_x, var_y), nations)
+        sns.scatterplot(x=df.iloc[:, 0], y=df.iloc[:, 1], data=df, hue="country", legend=True)
+        if var_x == "Year":
+            plt.xticks(range(2000, 2021))
+        plt.title(var_x + " vs " + var_y)
+        plt.show()
+
+    def scatterplot_three_variables(self, var_x: str, var_y: str, var_z: str, nations: [str]):
+        df = self.multi_dataframe((var_x, var_y, var_z), nations)
+        sns.scatterplot(x=df.iloc[:, 0], y=df.iloc[:, 1], size=df.iloc[:, 2], data=df, hue="country", legend=True)
+        if var_x == "Year":
+            plt.xticks(range(2000, 2021))
+        plt.title(var_x + " vs " + var_y + " with size: " + var_z)
+        plt.show()
+
+    # this function is called only from the main file to create the diagram.
+    # every information needed to create any diagram is given with the parameters
+    # this function also checks if the given parameters fit in the requested diagram
+    def create_plot(self, type: Type, variable, nations: [str], year=2000):
+        if type == Type.SCATTER:
+            if len(variable) == 2 and len(nations) > 0:
+                Diagram.scatterplot_two_variables(self, variable[0], variable[1], nations)
+            elif len(variable) == 3 and len(nations) > 0:
+                Diagram.scatterplot_three_variables(self, variable[0], variable[1], variable[2], nations)
+            else:
+                print("Scatterplots require 2-3 different variables, aswell as at least one country!")
+        elif type == Type.LINE:
+            if len(variable) == 1 and len(nations) > 0:
+                Diagram.lineplot_single_variable_over_time(self, variable=variable[0], nations=nations)
+            else:
+                print("Lineplots require one variable, aswell as at least one country!")
+        elif type == Type.BAR:
+            if len(variable) == 1 and len(nations) > 0:
+                Diagram.barplot_variable_over_time(self, variable=variable[0], nations=nations)
+            else:
+                print("Barplots require one variable, aswell as at least one country!")
+        elif type == Type.MAP:
+            year = int(year)
+            if 2000 <= year <= 2020 and len(nations) == 0:
+                print("Creating map. This might take a moment.")
+                Diagram.create_map(self, variable, year)
+            else:
+                print("Maps require one variable and a year from 2000-2020!")
+
+    # creates a Choropleth map for the given variable and year
+    # maps is saved before it's shown in a new browser window
+    def create_map(self, variable='% urban population', year=2000):
+        data = self.get_world_data(variable, year)
+        world_geo = geojson.load(open('countries.geojson'))
+        color4 = 'YlOrBr'
+        m = folium.Map(location=[35, 0], zoom_start=2.6)
+        folium.Choropleth(
+            geo_data=world_geo,
+            name="choropleth",
+            data=data,
+            columns=["ISO_A3", variable],
+            key_on='feature.properties.ISO_A3',
+            fill_color=color4,
+            legend_name=variable
+        ).add_to(m).geojson.add_child(folium.features.GeoJsonTooltip(['ADMIN'], labels=False))
+        m.save('map.html')
+        webbrowser.open_new('map.html')
---
+++ main.py
@@ -0,0 +1,222 @@
+import pandas as pd
+import dgrm
+
+from enum import Enum
+
+
+class Sheet(Enum):
+    WATER = 0
+    SANITATION = 1
+    HYGIENE = 2
+
+
+# initializes all variables, that are needed throughout a lot of functions
+def initialize_global_variables():
+    # variables_sanitation is a dictionary mapping each variable from the sanitation datasheet to
+    # its column index in the datasheet, to have easy integerbased access to the column
+    global variables_sanitation
+    variables_sanitation = {
+        "Country": 0, "ISO3": 1, "Year": 2, "Population (thousands)": 3,
+        "% urban population": 4, "% at least basic national sanitation": 5,
+        "limited (shared) national sanitation": 6, "unimproved national sanitation": 7,
+        "open defecation national sanitation": 8, "annual rate of change in basic national sanitation": 9,
+        "annual rate of change in open national defecation": 10, "at least basic rural sanitation": 11,
+        "limited (shared) rural sanitation": 12, "unimproved rural sanitation": 13,
+        "open defecation rural sanitation": 14, "annual rate of change in basic rural sanitation": 15,
+        "annual rate of change in open rural defecation": 16, "at least basic urban sanitation": 17,
+        "limited (shared) urban sanitation": 18, "unimproved urban sanitation": 19,
+        "open defecation urban sanitation": 20, "annual rate of change in basic urban sanitation": 21,
+        "annual rate of change in open urban defecation": 22,
+        "Safely managed national Proportion of population using improved sanitation facilities (excluding shared)": 23,
+        "Disposed in situ national Proportion of population using improved sanitation facilities (excluding shared)": 24,
+        "Emptied and treated national Proportion of population using improved sanitation facilities (excluding shared)": 25,
+        "Wastewater treated national Proportion of population using improved sanitation facilities (excluding shared)": 26,
+        "annual rate of change in safely mangaged national Proportion of population using improved sanitation facilities (excluding shared)": 27,
+        "Latriness and other national Proportion of population using improved sanitation facilities (including shared)": 28,
+        "Septic tanks national Proportion of population using improved sanitation facilities (including shared)": 29,
+        "Sewer connections national Proportion of population using improved sanitation facilities (including shared)": 30,
+        "Safely managed rural Proportion of population using improved sanitation facilities (excluding shared)": 31,
+        "Disposed in situ rural Proportion of population using improved sanitation facilities (excluding shared)": 32,
+        "Emptied and treated rural Proportion of population using improved sanitation facilities (excluding shared)": 33,
+        "Wastewater treated rural Proportion of population using improved sanitation facilities (excluding shared)": 34,
+        "Annual rate of change in safely managed rural Proportion of population using improved sanitation facilities (excluding shared)": 35,
+        "Latriness and other rural Proportion of population using improved sanitation facilities (including shared)": 36,
+        "Septic tanks rural Proportion of population using improved sanitation facilities (including shared)": 37,
+        "Sewer connections rural Proportion of population using improved sanitation facilities (including shared)": 38,
+        "Safely managed urban Proportion of population using improved sanitation facilities (excluding shared)": 39,
+        "Disposed in situ urban Proportion of population using improved sanitation facilities (excluding shared)": 40,
+        "Emptied and treated urban Proportion of population using improved sanitation facilities (excluding shared)": 41,
+        "Wastewater treated urban Proportion of population using improved sanitation facilities (excluding shared)": 42,
+        "Annual rate of change in safely managed urban Proportion of population using improved sanitation facilities (excluding shared)": 43,
+        "Latriness and other urban Proportion of population using improved sanitation facilities (including shared)": 44,
+        "Septic tanks urban Proportion of population using improved sanitation facilities (including shared)": 45,
+        "Sewer connections urban Proportion of population using improved sanitation facilities (including shared)": 46,
+        "SDG region": 47, "WHO region": 48, "UNICEF Programming region": 49, "UNICEF Reporting region": 50
+    }
+
+    # variables_water is a dictionary mapping each variable from the water datasheet to
+    # its column index in the datasheet, to have easy integerbased access to the column
+    global variables_water
+    variables_water = {"Country": 0, "ISO3": 1, "Year": 2, "Population (thousands)": 3, "% urban population": 4,
+                       "at least basic national water": 5, "limited national water": 6, "unimproved national water": 7,
+                       "surface water national": 8, "annual rate of change in basic national water": 9,
+                       "at least basic rural water": 10, "limited rural water": 11, "unimproved rural water": 12,
+                       "surface water rural": 13, "annual rate of change in basic rural water": 14,
+                       "at least basic urban water": 15, "limited urban water": 16, "unimproved urban water": 17,
+                       "surface water urban": 18, "annual rate of change in basic urban water": 19,
+                       "safely managed national proportion of population using improved water supplies": 20,
+                       "accessible on premises national proportion of population using improved water supplies": 21,
+                       "available when needed national proportion of population using improved water supplies": 22,
+                       "free from contamination national proportion of population using improved water supplies": 23,
+                       "annual rate of change in safely managed national proportion of population using improved water supplies": 24,
+                       "piped national proportion of population using improved water supplies": 25,
+                       "non-piped national proportion of population using improved water supplies": 26,
+                       "safely managed rural proportion of population using improved water supplies": 27,
+                       "accessible on premises rural proportion of population using improved water supplies": 28,
+                       "available when needed rural proportion of population using improved water supplies": 29,
+                       "free from contamination rural proportion of population using improved water supplies": 30,
+                       "annual rate of change in safely managed rural proportion of population using improved water supplies": 31,
+                       "piped rural proportion of population using improved water supplies": 32,
+                       "non-piped rural proportion of population using improved water supplies": 33,
+                       "safely managed urban proportion of population using improved water supplies": 34,
+                       "accessible on premises urban proportion of population using improved water supplies": 35,
+                       "available when needed urban proportion of population using improved water supplies": 36,
+                       "free from contamination urban proportion of population using improved water supplies": 37,
+                       "annual rate of change in safely managed urban proportion of population using improved water supplies": 38,
+                       "piped urban proportion of population using improved water supplies": 39,
+                       "non-piped urban proportion of population using improved water supplies": 40,
+                       "SDG region": 41, "WHO region": 42, "UNICEF programming region": 43,
+                       "UNICEF reporting region": 44
+                       }
+
+    # variables_hygiene is a dictionary mapping each variable from the hygiene datasheet to
+    # its column index in the datasheet, to have easy integerbased access to the column
+    global variables_hygiene
+    variables_hygiene = {"Country": 0, "ISO3": 1, "Year": 2, "Population (thousands)": 3, "% urban population": 4,
+                         "basic national hygiene": 5, "limited national hygiene": 6, "no facility national hygiene": 7,
+                         "annual rate of basic change in national hygiene": 8, "basic rural hygiene": 9,
+                         "limited rural hygiene": 10, "no facility rural hygiene": 11,
+                         "annual rate of change in basic rural hygiene": 12, "basic urban hygiene": 13,
+                         "limited urban hygiene": 14, "no facility urban hygiene": 15,
+                         "annual rate of change in basic urban sanitation": 16, "SDG region": 17, "WHO region": 18,
+                         "UNICEF programming region": 19, "UNICEF reporting region": 20
+                         }
+
+    read_data()
+
+
+# reads all the necessary data from csv files
+def read_data():
+    global dfWater
+    global dfSanitation
+    global dfHygiene
+    dfWater = pd.read_csv("Water_28_09_2021.csv", header=None)
+    dfSanitation = pd.read_csv("Sanitation_28_09_2021.csv", header=None)
+    dfHygiene = pd.read_csv("Hygiene_28_09_2021.csv", header=None)
+
+
+# prints every country
+def print_nations():
+    for country in dfSanitation.iloc[:, 0].unique().tolist():
+        print(country)
+    print("Just type any country that you want. Make sure it's spelled correctly.")
+
+
+# prints every variable from every sheet
+def print_variables():
+    print("Variables from the sanitation sheet:")
+    for key in variables_sanitation.keys():
+        print(key)
+    print("Variables from the water sheet:")
+    for key in variables_water.keys():
+        print(key)
+    print("Variables from the hygiene sheet:")
+    for key in variables_hygiene.keys():
+        print(key)
+    print(
+        "Just type any variable that you want. You don't have to specify the sheet it's from. Make sure it's spelled correctly.")
+
+
+# responsible for the CLI so that the user can select all the variables for a diagram
+def get_user_input(diag):
+    if input("Do you want to see every variable or only a few? every/few:") == "every":
+        print_variables()
+    else:
+        print("Population (thousands)")
+        print("% urban population")
+        print("basic national hygiene")
+        print("at least basic national water")
+        print("% at least basic national sanitation")
+    type = ""
+    while type != "scatterplot" and type != "barplot" and type != "lineplot" and type != "map":
+        type = input(
+            "Select the type of diagram you want to create. Available are scatterplot, barplot, lineplot and map:").lower()
+        if type != "scatterplot" and type != "barplot" and type != "lineplot" and type != "map":
+            print("Error: Unrecognized type!")
+    variables = []
+    if type == "scatterplot":
+        sum = input(
+            "Please choose whether you want two or three variables. The third one would determine the size of each individual point:")
+        variables.append(input("Please choose the first variable for the x axis:"))
+        variables.append(input("Please choose the second variable for the y axis:"))
+        if sum == 3 or sum == "3":
+            variables.append(input("Please choose the third variable for the size:"))
+    elif type == "barplot" or type == "lineplot":
+        variables.append(input("Please choose the second variable for the y axis:"))
+    elif type == "map":
+        variables.append(input("Please choose the variable:"))
+        year = input("Please choose the year (2000-2020):")
+        diag.create_plot(dgrm.Type.MAP, variable=variables[0], nations=[], year=year)
+    nations = []
+    print_nations()
+    if type != "map":
+        finished = False
+        while not finished:
+            if len(nations) > 0:
+                if nations[-1] == "finished":
+                    finished = True
+                    nations.remove("finished")
+            if not finished:
+                nations.append(str(input(
+                    "Please select a country you want to add. Type 'average' for the worldwide average. Type 'finished' when you are finished:")))
+        if type == "scatterplot":
+            diag.create_plot(dgrm.Type.SCATTER, variables, nations)
+        elif type == "barplot":
+            diag.create_plot(dgrm.Type.BAR, variables, nations)
+        elif type == "lineplot":
+            diag.create_plot(dgrm.Type.LINE, variables, nations)
+
+
+# creates csv files from excelfiles, shouldnt be needed ever again, didnt delete for completeness
+def excel_to_csv():
+    read_file = pd.read_excel('WHS_11_08_2021.xlsx', sheet_name='Sanitation')
+    read_file.to_csv(r'C:\Users\schoe\PycharmProjects\testing\Sanitation_12_08_2021.csv', index=None, header=True)
+
+
+# currently cleans water, shouldnt be needed ever again, didnt delete for completeness
+def clean_data():
+    global dfHygiene
+    dfHygiene = dfHygiene.replace("<1", "0.1")
+    dfHygiene = dfHygiene.replace(">99", "99.9")
+    dfHygiene = dfHygiene.replace("-", "0")
+    dfHygiene = dfHygiene.fillna(0)
+    dfHygiene.to_csv("Hygiene_28_09_2021.csv")
+
+
+# pretty much the main function, but havent googled yet on how to do it properly
+def run_program():
+    initialize_global_variables()
+    diag = dgrm.Diagram(
+        type=dgrm.Type.MAP,
+        df_s=dfSanitation,
+        df_w=dfWater,
+        df_h=dfHygiene,
+        var_s=variables_sanitation,
+        var_w=variables_water,
+        var_h=variables_hygiene
+    )
+    get_user_input(diag)
+
+
+print('hello')
+run_program()
